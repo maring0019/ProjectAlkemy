2a5627094bce50e020ec1e3a650edbc6c3ba084e
Jonatan Fontana <fontanajonatan1133@gmail.com>
2021-06-23 19:59:16 -0300

    'update'

---
+++ src/main/java/com/alkemy/ong/Repository/RolRepository.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.Repository;
+
+import com.alkemy.ong.Model.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol,Long> {
+
+}
