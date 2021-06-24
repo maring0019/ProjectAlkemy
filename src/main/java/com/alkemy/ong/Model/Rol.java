ffec0c14d2d2260cea04f2b128c9cef093f60d2f
Jonatan Fontana <fontanajonatan1133@gmail.com>
2021-06-24 11:32:43 -0300

    '@notempty/saque_pom'

--- src/main/java/com/alkemy/ong/Model/Rol.java
+++ src/main/java/com/alkemy/ong/Model/Rol.java
@@ -1,12 +1,14 @@
 package com.alkemy.ong.Model;
 
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -27,6 +29,7 @@ public class Rol implements Serializable {
     private Long id;
 
     @Column(nullable = false,length = 80)
+    @NotEmpty(message = "el campo name no puede estar vacio")
     private String name;
 
     private String description;
