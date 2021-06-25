0ecb210bdaa8cbe1d93988c94c12f48df94a6803
Federico <soyfdherrera@gmail.com>
2021-06-25 10:04:45 -0300

    OT45-30

---
+++ src/main/java/com/alkemy/ong/util/EmailConstants.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.util;
+
+public class EmailConstants {
+
+    public static final String ENCODING = "utf-8";
+    public static final String SUCCESS = "¡Registro exitóso!";
+    public static final String SENT_FROM = "contacto@alkemy.com";
+
+}
