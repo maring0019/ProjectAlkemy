3966f2a7f03545c57b16353d74c84301550a757c
Rodrigo Munafo <rodymuba@gmail.com>
2021-06-25 19:51:29 -0300

    Se actualizo modelo de testimonio Interface e Implementacion

--- src/main/java/com/alkemy/ong/service/Interface/ITestimonials.java
+++ src/main/java/com/alkemy/ong/service/Interface/ITestimonials.java
@@ -1,5 +1,20 @@
 package com.alkemy.ong.service.Interface;
 
 
+import com.alkemy.ong.model.Testimonials;
+
+import java.util.List;
+
 public interface ITestimonials {
+
+    void Save(Testimonials testimonial);
+
+    void Edit(Testimonials testimonial, Long id);
+
+    List<Testimonials> searchAll();
+
+    Testimonials searchById(Long id);
+
+    void remove(Long id);
+
 }
--- src/main/java/com/alkemy/ong/service/impl/TestimonialsServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/TestimonialsServiceImpl.java
@@ -1,6 +1,70 @@
 package com.alkemy.ong.service.impl;
+import com.alkemy.ong.model.Testimonials;
+import com.alkemy.ong.repository.TestimonialsRepository;
 import com.alkemy.ong.service.Interface.ITestimonials;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
 
 
 public class TestimonialsServiceImpl implements ITestimonials {
+
+    @Autowired
+
+    private TestimonialsRepository testimonialsRepo;
+
+    @Override
+
+    public void Save(Testimonials testimonial) {
+
+        testimonialsRepo.save(testimonial);
+
+    }
+
+
+    @Override
+
+    public void Edit(Testimonials testimonial, Long id) {
+
+        testimonial.setId(id);
+
+        testimonialsRepo.save(testimonial);
+
+    }
+
+
+    @Override
+
+    public List<Testimonials> searchAll() {
+
+        return testimonialsRepo.findAll();
+
+    }
+
+
+    @Override
+
+    public Testimonials searchById(Long id) {
+
+        Optional<Testimonials> optional = testimonialsRepo.findById(id);
+
+        if(optional.isPresent()){
+
+            return optional.get();
+
+        }
+
+        return null;
+
+    }
+
+
+    @Override
+
+    public void remove(Long id) {
+
+        testimonialsRepo.deleteById(id);
+
+    }
 }
