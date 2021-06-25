f657ca16aef7a9c5c48631980848d6ea7a659243
Gabriel Andino <gabriel_andino@outlook.com>
2021-06-25 20:08:52 -0300

    Adding the user controller with patch and put method

--- src/main/java/com/alkemy/ong/controller/UsersController.java
+++ src/main/java/com/alkemy/ong/controller/UsersController.java
@@ -1,8 +1,48 @@
 package com.alkemy.ong.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.alkemy.ong.dto.UsersDto;
+import com.alkemy.ong.service.Interface.IUsersService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.json.JsonPatch;
+import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/users")
+@AllArgsConstructor
 public class UsersController {
+
+    @Autowired
+    private final IUsersService usersService;
+
+    @Autowired
+    private final ModelMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<Object> createUser(@Valid @RequestBody UsersDto usersDto) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(usersService.createUser(usersDto));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
+
+    }
+
+    @PatchMapping(path = "/{id}", consumes = "application/json-patch+json")
+    public ResponseEntity<Object> updateUser(@PathVariable Long id, @RequestBody JsonPatch patch) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(usersService.patchUpdate(id, patch));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
 
 }
