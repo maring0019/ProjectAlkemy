3d5d71a02ae8e2d9888ff639d1ff3d534fc78a11
Gabriel Andino <gabriel_andino@outlook.com>
2021-06-25 20:01:20 -0300

    Ignoring the user password, created and edited were added

--- src/main/java/com/alkemy/ong/dto/UsersDto.java
+++ src/main/java/com/alkemy/ong/dto/UsersDto.java
@@ -1,14 +1,18 @@
 package com.alkemy.ong.dto;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
 import com.alkemy.ong.model.Role;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter @Setter
 public class UsersDto implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -20,12 +24,24 @@ public class UsersDto implements Serializable{
 	private String lastName;
 	
 	private String email;
-	
+
 	private String password;
 	
 	private String photo;
-	
-	private Set<Role> roles = new HashSet<>();
-	
 
+	private Date created;
+
+	private Date edited;
+	
+	private Set<Role> roles;
+
+	@JsonIgnore
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
