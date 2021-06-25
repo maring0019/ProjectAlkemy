ddb33c911a44413b6c50b6b32d7c6fd680c05b15
EliasFranco <eliaass95@gmail.com>
2021-06-25 19:04:39 -0300

    endpoint organizations

--- src/main/java/com/alkemy/ong/controller/OrganizationController.java
+++ src/main/java/com/alkemy/ong/controller/OrganizationController.java
@@ -1,2 +1,20 @@
-package com.alkemy.ong.controller;public class OrganizationController {
+package com.alkemy.ong.controller;
+
+import com.alkemy.ong.dto.OrganizationDto;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class OrganizationController {
+
+    @RequestMapping(value = "/organization/public", method = RequestMethod.GET)
+    public ResponseEntity<?> getOrganizationData(){
+
+    }
 }
--- src/main/java/com/alkemy/ong/dto/OrganizationDto.java
+++ src/main/java/com/alkemy/ong/dto/OrganizationDto.java
@@ -1,2 +1,21 @@
-package com.alkemy.ong.dto;public class OrganizationDto {
+package com.alkemy.ong.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+public class OrganizationDto {
+
+    private String name;
+
+    private String image;
+
+    private String phone;
+
+    private String address;
 }
