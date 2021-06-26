29443269c911bdda67428cf1fcca411f0ced8ee7
Federico <soyfdherrera@gmail.com>
2021-06-26 03:08:13 -0300

    OT45-30 Desarrollado en conjunto con Maxi Araujo

--- src/main/java/com/alkemy/ong/util/EmailConstants.java
+++ src/main/java/com/alkemy/ong/util/EmailConstants.java
@@ -1,11 +1,9 @@
 package com.alkemy.ong.util;
 
 public class EmailConstants {
-
     public static final String EMAIL_TEMPLATE = "templates/register_email.html";
     public static final String EMAIL_SUBJECT = "¡Registro Exitoso!";
     public static final String EMAIL_FROM = "contacto@alkemy.com";
-    public static final String API_KEY = "${app.sendgrid.key}";
-    public static final String EMAIL_TYPE = "html";
+    public static final String EMAIL_TYPE = "text/html";
     public static final String EMAIL_ENDPOINT = "mail/send";
 }
