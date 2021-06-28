238f55b0a743dd3a840f9345cfb28c2f9f501164
maxiAraujo <maxy_093@hotmail.com>
2021-06-28 12:58:57 -0300

    arreglar conflictos

--- src/main/java/com/alkemy/ong/config/AppConfiguration.java
+++ src/main/java/com/alkemy/ong/config/AppConfiguration.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.alkemy.ong.service.impl.UsersServiceImpl;
+
 @Configuration
 public class AppConfiguration {
 
@@ -12,6 +14,12 @@ public class AppConfiguration {
     public ModelMapper modelMapper() {
         ModelMapper mapper = new ModelMapper();
         return mapper;
+    }
+
+    @Bean
+    public UsersServiceImpl userServiceImpl() {
+        UsersServiceImpl userServiceImpl = new UsersServiceImpl();
+        return userServiceImpl;
     }
 
 }
