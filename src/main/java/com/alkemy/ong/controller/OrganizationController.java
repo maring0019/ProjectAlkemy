030a3dfa7d6d023d00b18737d46a6d8bf0e85c9b
daxhaye <daxhaye@gmail.com>
2021-06-30 14:47:45 -0300

    EndPoint updateOrganization

--- src/main/java/com/alkemy/ong/controller/OrganizationController.java
+++ src/main/java/com/alkemy/ong/controller/OrganizationController.java
@@ -1,24 +1,36 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.OrganizationDto;
+import com.alkemy.ong.model.Organization;
 import com.alkemy.ong.service.impl.OrganizationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+import javax.validation.Valid;
+
+@RestController("/organization")
 public class OrganizationController {
 
     @Autowired
     OrganizationServiceImpl organizationService;
 
-    @GetMapping(value = "/organization/public")
+    @GetMapping(value = "/public")
     public ResponseEntity<?> getOrganizationData(){
         List<OrganizationDto> dto = organizationService.getAll();
         return ResponseEntity.ok(dto);
+    }
+    
+    @PostMapping("/public")
+    public ResponseEntity<?> updateOrganization(@Valid Organization organization) {
+    	try {
+    		
+    	}
+    	
     }
 
 }
