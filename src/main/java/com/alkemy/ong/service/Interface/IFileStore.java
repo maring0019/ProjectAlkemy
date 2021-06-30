1ecf0b7c6461f2f5e96a5d568ccac502115fcea3
Gabriel Andino <gabriel_andino@outlook.com>
2021-06-30 14:12:29 -0300

    Moving some internal methods to the FileStore interfae

--- src/main/java/com/alkemy/ong/service/Interface/IFileStore.java
+++ src/main/java/com/alkemy/ong/service/Interface/IFileStore.java
@@ -2,21 +2,37 @@ package com.alkemy.ong.service.Interface;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import static org.apache.http.entity.ContentType.*;
+
 public interface IFileStore {
 
-    void save(String path, String fileName, Optional<Map<String, String>> optionalMetadata, InputStream inputStream);
+    void save(String path, String fileName, MultipartFile file);
 
     byte[] download(String path, String key);
 
-    Map<String, String> extractMetadata(MultipartFile file);
+    default Optional<Map<String, String>> extractMetadata(MultipartFile file) {
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+        return Optional.of(metadata);
+    }
 
-    void isAnImage(MultipartFile file);
+    default void isAnImage(MultipartFile file) {
+        if(!Arrays.asList(IMAGE_JPEG.getMimeType(), IMAGE_PNG.getMimeType(), IMAGE_GIF.getMimeType()).contains(file.getContentType())) {
+            throw new IllegalStateException("El archivo debe ser una imagen valida (JPEG, JPG, PNG, GIF).");
+        }
+    }
 
-    void fileIsEmpty(MultipartFile file);
+    default void fileIsEmpty(MultipartFile file) {
+        if(file.isEmpty()) {
+            throw new IllegalStateException("No se puede subir un archivo vacio.");
+        }
+    }
 
     void deleteFilesFromS3Bucket(String fileUrl);
 
