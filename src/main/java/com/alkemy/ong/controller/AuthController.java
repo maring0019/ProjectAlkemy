748b0e80d404ee32fb7533b3bbaf69b578a659f3
Antonio Gabriel <4antoniogabriel20@gmail.com>
2021-06-30 01:26:45 +0000

    Merged in SpDg45-36 (pull request #28)
    SpDg45 36
    
    * change method getToken to public
    
    * Added endPoint auth/me info User
    
    * update from develop
    
    Approved-by: Yamil Amado

--- src/main/java/com/alkemy/ong/controller/AuthController.java
+++ src/main/java/com/alkemy/ong/controller/AuthController.java
@@ -1,15 +1,28 @@
 package com.alkemy.ong.controller;
 
-import com.alkemy.ong.dto.UsersDto;
-import com.alkemy.ong.service.Interface.IUsersService;
-import lombok.AllArgsConstructor;
+import javax.json.JsonPatch;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.json.JsonPatch;
-import javax.validation.Valid;
+import com.alkemy.ong.dto.UsersDto;
+import com.alkemy.ong.jwt.JwtFilter;
+import com.alkemy.ong.jwt.JwtProvider;
+import com.alkemy.ong.service.Interface.IUsersService;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,6 +31,10 @@ public class AuthController {
 
     @Autowired
     private final IUsersService usersService;
+    @Autowired
+    private JwtProvider jwtProvider;
+    @Autowired
+    private JwtFilter jwtFilter;
 
 
     @PostMapping(path = "/register")
@@ -47,6 +64,19 @@ public class AuthController {
     		return ResponseEntity.ok().build();
     	} catch (Exception e) {
     		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    	}
+    }
+    
+    @GetMapping("/me")
+    public ResponseEntity<Object> userInfo(HttpServletRequest request){
+    	try {
+    		String token = jwtFilter.getToken(request);
+    		String email = jwtProvider.getEmailFromToken(token);
+   	
+			return new ResponseEntity<>(usersService.loadUserByUsername(email), HttpStatus.FOUND);
+
+    	}catch(UsernameNotFoundException e) {
+    		return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     	}
     }
 }
