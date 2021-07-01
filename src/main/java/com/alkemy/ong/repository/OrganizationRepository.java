04a04bf757e82e9684430092d72edeace3e0723d
maxiAraujo <maxy_093@hotmail.com>
2021-07-01 19:33:05 -0300

    volver a resolver conflictos

--- src/main/java/com/alkemy/ong/repository/OrganizationRepository.java
+++ src/main/java/com/alkemy/ong/repository/OrganizationRepository.java
@@ -2,8 +2,9 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface OrganizationRepository extends JpaRepository<Organization,Long> {
-
-
+@Repository
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
 }
+
