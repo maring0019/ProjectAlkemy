365c61737ba5d70e9ab63bb9f4d48ffc16230523
Gabriel Andino <gabriel_andino@outlook.com>
2021-07-06 15:38:36 -0300

    Adding a dto for image slides

---
+++ src/main/java/com/alkemy/ong/dto/ImageSlideDto.java
@@ -0,0 +1,21 @@
+package com.alkemy.ong.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter @Setter
+public class ImageSlideDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String imageUrl;
+    private String text;
+    private Long organizationId;
+    private Long ordered;
+    private Date createdAt;
+
+}
