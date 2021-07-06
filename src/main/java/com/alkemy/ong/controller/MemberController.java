f41e0e1ffaa89cef3aa9e9f18e611fb066c1afb0
Jonatan Fontana <fontanajonatan1133@gmail.com>
2021-07-06 14:46:35 -0300

    'update'

--- src/main/java/com/alkemy/ong/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.model.Member;
 import com.alkemy.ong.service.Interface.IMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +20,8 @@ public class MemberController {
     private IMemberService iMemberService;
 
     @GetMapping("/members")
-    public List<Member> getAllMembers(){
-        return iMemberService.showAllMembers();
+    public ResponseEntity< List<Member> > getAllMembers(){
+        return ResponseEntity.status(HttpStatus.OK).body(iMemberService.showAllMembers());
     }
 
 
