9fdbf6c3ee3d8c30f303dbccf5281a5ac060e39f
Gabriel Andino <gabriel_andino@outlook.com>
2021-07-06 15:42:14 -0300

    Adding findById and FindAllByOrganizationId queries

--- src/main/java/com/alkemy/ong/repository/ImageSlideRepository.java
+++ src/main/java/com/alkemy/ong/repository/ImageSlideRepository.java
@@ -1,9 +1,18 @@
 package com.alkemy.ong.repository;
 
+import com.alkemy.ong.dto.ImageSlideDto;
 import com.alkemy.ong.model.ImageSlide;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ImageSlideRepository extends JpaRepository<ImageSlide, Long> {
+
+    List<ImageSlide> findAllByOrganizationIdOrderByOrdered(Long organizationId);
+
+    Optional<ImageSlide> findById(Long id);
+
 }
