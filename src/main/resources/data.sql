65414e906a647ebd2c776967a34e059a648f7d67
Mariela Benicio <maring0019@gmail.com>
2021-07-05 23:12:49 -0300

    Updating gitignore

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Role (id, created_date, deleted, description, edited_date, name, roleName) VALUES
+
+(1,null,0,null,null,'ROLE_USER','ROLE_USER'),
+(2,null,0,null,null,'ROLE_ADMIN','ROLE_ADMIN') ;
