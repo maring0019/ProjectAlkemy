471634808bfd3a899eeb79679c0d248ebe3d328b
EliasFranco <eliaass95@gmail.com>
2021-07-06 22:09:43 -0300

    creado clase, repositorio y servicio de Contactos

---
+++ src/main/java/com/alkemy/ong/service/Interface/IContacts.java
@@ -0,0 +1,7 @@
+package com.alkemy.ong.service.Interface;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface IContacts {
+}
---
+++ src/main/java/com/alkemy/ong/service/impl/ContactsServiceImpl.java
@@ -0,0 +1,6 @@
+package com.alkemy.ong.service.impl;
+
+import com.alkemy.ong.service.Interface.IContacts;
+
+public class ContactsServiceImpl implements IContacts {
+}
