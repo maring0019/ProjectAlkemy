e507da340071ae71141f6dff141a364d0b1facab
Rodrigo Munafo <rodymuba@gmail.com>
2021-07-07 10:36:55 -0300

    Se agrega endopoint de detalles para slides, se Agrega findget en controlador, se agrega impl en service getImageSlideById , se agrega @ManytoOne en Slide a Organization

--- src/main/java/com/alkemy/ong/repository/ImageSlideRepository.java
+++ src/main/java/com/alkemy/ong/repository/ImageSlideRepository.java
@@ -4,6 +4,9 @@ import com.alkemy.ong.model.ImageSlide;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ImageSlideRepository extends JpaRepository<ImageSlide, Long> {
+    Optional<ImageSlide> findById(Long id);
 }
