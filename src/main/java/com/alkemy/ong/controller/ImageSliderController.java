0a2dd08367b7893cab4cd22321c6951be122efac
Gabriel Andino <gabriel_andino@outlook.com>
2021-07-07 23:04:32 -0300

    Adding the delete slide image endpoint

--- src/main/java/com/alkemy/ong/controller/ImageSliderController.java
+++ src/main/java/com/alkemy/ong/controller/ImageSliderController.java
@@ -1,16 +1,12 @@
 package com.alkemy.ong.controller;
 
 
-import com.alkemy.ong.dto.ImageSlideDto;
-import com.alkemy.ong.model.ImageSlide;
+import com.alkemy.ong.dto.ImageSlideCreationDto;
 import com.alkemy.ong.service.Interface.IImgSlideService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
 
 
 @RestController
@@ -24,10 +20,11 @@ public class ImageSliderController {
         this.iImgSlideService = iImgSlideService;
     }
 
+
     @PostMapping
-    public ResponseEntity<Object> createImageSlide(@Valid @RequestBody ImageSlideDto imageSlideDto) {
+    public ResponseEntity<Object> createImageSlide(@ModelAttribute ImageSlideCreationDto imageSlideCreationDto) {
         try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(iImgSlideService.createSlide(imageSlideDto));
+            return ResponseEntity.status(HttpStatus.CREATED).body(iImgSlideService.createSlide(imageSlideCreationDto));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
@@ -41,4 +38,15 @@ public class ImageSliderController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<String> deleteSlide(@PathVariable Long id) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(iImgSlideService.deleteImage(id));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
 }
