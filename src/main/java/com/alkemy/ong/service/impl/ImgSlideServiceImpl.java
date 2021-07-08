0f42dda02414e8a0781304a08b38862858d1873d
Gabriel Andino <gabriel_andino@outlook.com>
2021-07-07 23:03:42 -0300

    Adding the delete method in implementation service

--- src/main/java/com/alkemy/ong/service/impl/ImgSlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/ImgSlideServiceImpl.java
@@ -1,19 +1,22 @@
 package com.alkemy.ong.service.impl;
 
+import com.alkemy.ong.dto.ImageSlideCreationDto;
 import com.alkemy.ong.dto.ImageSlideDto;
 import com.alkemy.ong.exception.InvalidImageException;
-import com.alkemy.ong.exception.InvalidUserException;
 import com.alkemy.ong.model.ImageSlide;
 import com.alkemy.ong.model.Organization;
 import com.alkemy.ong.repository.ImageSlideRepository;
 import com.alkemy.ong.repository.OrganizationRepository;
+import com.alkemy.ong.service.Interface.IFileStore;
 import com.alkemy.ong.service.Interface.IImgSlideService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.EntityNotFoundException;
 import java.sql.Date;
@@ -21,6 +24,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,68 +34,63 @@ public class ImgSlideServiceImpl implements IImgSlideService {
     private final MessageSource messageSource;
     private final ModelMapper mapper;
     private final OrganizationRepository organizationRepository;
+    private final IFileStore fileStore;
+
+    @Value("${aws.s3.bucket.name}")
+    private String bucketName;
+
+    @Value("${aws.s3.bucket.endpointUrl}")
+    private String bucketUrl;
 
     @Autowired
-    public ImgSlideServiceImpl(ImageSlideRepository imageRepo, MessageSource messageSource, ModelMapper mapper, OrganizationRepository organizationRepository) {
+    public ImgSlideServiceImpl(ImageSlideRepository imageRepo, MessageSource messageSource, ModelMapper mapper, OrganizationRepository organizationRepository, IFileStore fileStore) {
         this.imageRepo = imageRepo;
         this.messageSource = messageSource;
         this.mapper = mapper;
         this.organizationRepository = organizationRepository;
+        this.fileStore = fileStore;
     }
 
 
     @Override
-    public ImageSlideDto createSlide(ImageSlideDto imageSlideDto) {
-        ImageSlide imageSlideCreated = new ImageSlide(
-                imageSlideDto.getImageUrl(),
-                imageSlideDto.getText(),
-                imageSlideDto.getOrdered(),
-                imageSlideDto.getOrganizationId(),
+    public ImageSlideDto createSlide(ImageSlideCreationDto imageSlideCreationDto) {
+        ImageSlide imageSlideEntity = new ImageSlide(
+                imageSlideCreationDto.getText(),
+                imageSlideCreationDto.getOrdered(),
+                imageSlideCreationDto.getOrganizationId(),
                 Date.from(Instant.now())
         );
+        System.out.println(imageSlideEntity.getOrdered());
+        ImageSlide imageSlideCreated = imageRepo.save(imageSlideEntity);
+        uploadImage(imageSlideCreationDto, imageSlideCreated);
         return mapper.map(imageRepo.save(imageSlideCreated), ImageSlideDto.class);
     }
 
 
-    public ImageSlideDto addImage(ImageSlideDto image) throws InvalidImageException, InvalidUserException {
-        if (userIsAuthorized()) {
-            if(doNotAdd(image))
-                throw new InvalidImageException(messageSource.getMessage("slide.error.exists.img",null, Locale.getDefault()));
-            ImageSlide newImage = new ImageSlide(
-                    image.getImageUrl(), image.getText(), image.getOrdered(), image.getOrganizationId(), Date.from(Instant.now())
-            );
-            return mapper.map(imageRepo.save(newImage), ImageSlideDto.class);
-        } else {
-            throw new InvalidUserException(messageSource.getMessage("slide.error.invalid.user", null, Locale.getDefault()));
-        }
-    }
-
-
-
+    @Override
     public List<ImageSlide> getAll() {
         return imageRepo.findAll().stream().sorted().collect(Collectors.toList());
     }
 
-    public ImageSlideDto updateImage(Long id, ImageSlideDto image) throws InvalidImageException {
-        if(doNotAdd(image))
-            throw new InvalidImageException(messageSource.getMessage("slide.error.exists.img",null, Locale.getDefault()));
+    @Override
+    public ImageSlideCreationDto updateImage(Long id, ImageSlideCreationDto image) throws InvalidImageException {
 
         ImageSlide imageSlide = getImageSlideById(id);
         imageSlide.setOrdered(image.getOrdered());
         imageSlide.setText(image.getText());
-        imageSlide.setImageUrl(image.getImageUrl());
         imageSlide.setOrganizationId(image.getOrganizationId());
-        return mapper.map(imageRepo.save(imageSlide), ImageSlideDto.class);
-    }
-
-    public void deleteImage(Long id){
-        ImageSlide found = imageRepo.findById(id).orElseThrow(() -> new IllegalStateException(messageSource.getMessage("slide.error.do.not.exists",null, Locale.getDefault())));
-        if(found != null)
-            imageRepo.deleteById(id);
+        return mapper.map(imageRepo.save(imageSlide), ImageSlideCreationDto.class);
     }
 
     @Override
-    public List<ImageSlideDto> getAllSlidesByOrganization(Long organizationId) {
+    public String deleteImage(Long id){
+        ImageSlide imageSlide = getImageSlideById(id);
+        imageRepo.delete(imageSlide);
+        return messageSource.getMessage("slide.delete.successful", null, Locale.getDefault());
+    }
+
+    @Override
+    public List<ImageSlideCreationDto> getAllSlidesByOrganization(Long organizationId) {
 
         //Validamos que exista una organizacion con el id especificado
         Organization organization = organizationRepository.findById(organizationId).orElseThrow(() -> new EntityNotFoundException(
@@ -99,20 +98,23 @@ public class ImgSlideServiceImpl implements IImgSlideService {
         ));
 
         List<ImageSlide> imageSlides = imageRepo.findAllByOrganizationIdOrderByOrdered(organization.getId());
-        List<ImageSlideDto> imageSlideDtos = new ArrayList<>();
-        imageSlides.forEach(i -> imageSlideDtos.add(mapper.map(i, ImageSlideDto.class)));
-        return imageSlideDtos;
+        List<ImageSlideCreationDto> imageSlideCreationDtos = new ArrayList<>();
+        imageSlides.forEach(i -> imageSlideCreationDtos.add(mapper.map(i, ImageSlideCreationDto.class)));
+        return imageSlideCreationDtos;
     }
 
     @Override
     public ImageSlide getImageSlideById(Long id) {
         return imageRepo.findById(id).orElseThrow(() -> new EntityNotFoundException(
-                messageSource.getMessage("slide.error.do.not.exists",null, Locale.getDefault())
+                messageSource.getMessage("slide.error.not.found",null, Locale.getDefault())
         ));
     }
 
-    private boolean doNotAdd(ImageSlideDto image){
-        return getAll().stream().anyMatch(savedImage -> savedImage.getImageUrl().equals(image.getImageUrl()));
+    private void uploadImage(ImageSlideCreationDto imageSlideCreationDto, ImageSlide imageSlide) {
+        String path = String.format("%s/%s", bucketName, "Image-Slide-" + imageSlide.getId());
+        String filename = String.format("%s-%s", imageSlideCreationDto.getImage().getOriginalFilename(), UUID.randomUUID());
+        fileStore.save(path, filename, imageSlideCreationDto.getImage());
+        imageSlide.setImageUrl(path+filename);
     }
 
 
