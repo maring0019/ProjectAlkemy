ad4ce629f5afaed38b0cbfde6eddefa1bb0fbebf
Gabriel Andino <gabriel_andino@outlook.com>
2021-07-08 17:18:33 -0300

    Adding the imageslide creation dto

--- src/main/java/com/alkemy/ong/dto/ImageSlideCreationDto.java
+++ src/main/java/com/alkemy/ong/dto/ImageSlideCreationDto.java
@@ -5,18 +5,15 @@ import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
-import java.util.Date;
 
 @Getter @Setter
-public class ImageSlideDto implements Serializable {
+public class ImageSlideCreationDto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private Long id;
     private MultipartFile image;
     private String text;
     private Long organizationId;
     private Long ordered;
-    private Date createdAt;
 
 }
