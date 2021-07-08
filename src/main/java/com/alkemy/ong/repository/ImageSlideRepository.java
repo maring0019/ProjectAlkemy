b8cb33ed0059f8c53fc343499cceffc82c03d5e0
Gabriel Andino <gabriel_andino@outlook.com>
2021-07-08 17:21:04 -0300

    Adding optional findById

--- src/main/java/com/alkemy/ong/repository/ImageSlideRepository.java
+++ src/main/java/com/alkemy/ong/repository/ImageSlideRepository.java
@@ -1,6 +1,5 @@
 package com.alkemy.ong.repository;
 
-import com.alkemy.ong.dto.ImageSlideDto;
 import com.alkemy.ong.model.ImageSlide;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
