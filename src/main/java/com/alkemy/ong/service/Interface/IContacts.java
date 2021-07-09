c18cb07302c62b7c781387f04c192eaadc7d4432
EliasFranco <eliaass95@gmail.com>
2021-07-09 12:54:31 -0300

    creado controller, endpoint para almacenar contactos

--- src/main/java/com/alkemy/ong/service/Interface/IContacts.java
+++ src/main/java/com/alkemy/ong/service/Interface/IContacts.java
@@ -1,6 +1,9 @@
 package com.alkemy.ong.service.Interface;
 
+import com.alkemy.ong.dto.ContactsDto;
 import org.springframework.stereotype.Service;
 
 public interface IContacts {
+
+    public ContactsDto createContacts(ContactsDto dto);
 }
