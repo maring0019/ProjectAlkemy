3eddf4218b0312502b0b4d5be1e428cdf2298fe5
Gabriel Andino <gabriel_andino@outlook.com>
2021-07-12 23:15:31 -0300

    Adding exception to @ModelAttribute

--- src/main/java/com/alkemy/ong/exception/AppExceptionsHandler.java
+++ src/main/java/com/alkemy/ong/exception/AppExceptionsHandler.java
@@ -1,7 +1,9 @@
 package com.alkemy.ong.exception;
 
 import com.alkemy.ong.util.CustomFieldError;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import org.springframework.web.context.request.WebRequest;
 
+import javax.resource.spi.InvalidPropertyException;
 import java.util.*;
 
 @ControllerAdvice
@@ -17,6 +20,22 @@ public class AppExceptionsHandler {
     //Excepciones al validar un modelo en el controller
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
     public ResponseEntity<Object> handleArgumentNotValid(MethodArgumentNotValidException ex, WebRequest webRequest) {
+
+        final List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        final List<CustomFieldError> customFieldErrors = new ArrayList<>();
+
+        for (FieldError fieldError : fieldErrors) {
+            final String field = fieldError.getField();
+            final String message = fieldError.getDefaultMessage();
+            final CustomFieldError customFieldError = CustomFieldError.builder().field(field).message(message).build();
+            customFieldErrors.add(customFieldError);
+        }
+
+        return ResponseEntity.badRequest().body(customFieldErrors);
+    }
+
+    @ExceptionHandler(value = {BindException.class})
+    public ResponseEntity<Object> handleBindException(BindException ex, WebRequest webRequest) {
 
         final List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
         final List<CustomFieldError> customFieldErrors = new ArrayList<>();
