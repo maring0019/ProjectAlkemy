9f0bfa320dc584752e6e39f57524b9c2b96d492d
Gabriel Andino <gabriel_andino@outlook.com>
2021-07-12 23:25:13 -0300

    Adding the upload image method and the pagination method

--- src/main/java/com/alkemy/ong/service/impl/NewsServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/NewsServiceImpl.java
@@ -1,41 +1,55 @@
 package com.alkemy.ong.service.impl;
 
-import com.alkemy.ong.dto.NewsDto;
+import com.alkemy.ong.dto.NewsCreationDto;
 import com.alkemy.ong.dto.NewsResponseDto;
 import com.alkemy.ong.model.Categories;
 import com.alkemy.ong.model.News;
 import com.alkemy.ong.repository.NewsRepository;
+import com.alkemy.ong.service.Interface.ICategories;
+import com.alkemy.ong.service.Interface.IFileStore;
 import com.alkemy.ong.service.Interface.INews;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.stereotype.Service;
 import java.util.Date;
 
 
 import org.springframework.context.MessageSource;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.persistence.EntityNotFoundException;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.UUID;
 
 
 @Service
-public class NewsImpl implements INews {
+public class NewsServiceImpl implements INews {
 
     private final NewsRepository newsRepository;
-    private final  ModelMapper mapper;
+    private final ProjectionFactory projectionFactory;
     private final MessageSource messageSource;
+    private final IFileStore fileStore;
+    private final ICategories categoriesService;
+    @Value("${aws.s3.bucket.name}")
+    private String bucketName;
+    @Value("${aws.s3.bucket.endpointUrl}")
+    private String bucketUrl;
 
     @Autowired
-    public NewsImpl(NewsRepository newsRepository, ModelMapper mapper, MessageSource messageSource) {
+    public NewsServiceImpl(NewsRepository newsRepository, ProjectionFactory projectionFactory, MessageSource messageSource, IFileStore fileStore, ICategories categoriesService) {
         this.newsRepository = newsRepository;
-        this.mapper = mapper;
+        this.projectionFactory = projectionFactory;
         this.messageSource = messageSource;
+        this.fileStore = fileStore;
+        this.categoriesService = categoriesService;
     }
 
 
@@ -53,45 +67,41 @@ public class NewsImpl implements INews {
     }
 
     @Override
-    public NewsDto save(NewsDto newsDto) {
+    public NewsResponseDto save(NewsCreationDto newsCreationDto) {
+
+        Categories categoriesEntity = categoriesService.findCategoriesById(newsCreationDto.getCategory());
+
         News newsEntity = News.builder()
-                .name(newsDto.getName())
-                .content(newsDto.getContent())
-                .image(newsDto.getImage())
-                .category(mapper.map(newsDto.getCategory(), Categories.class))
+                .name(newsCreationDto.getName())
+                .content(newsCreationDto.getContent())
+                .category(categoriesEntity)
                 .build();
-        return mapper.map(newsRepository.save(newsEntity), NewsDto.class);
+        News newsCreated = newsRepository.save(newsEntity);
+        uploadImage(newsCreationDto.getImage(), newsCreated);
+        return projectionFactory.createProjection(NewsResponseDto.class, newsRepository.save(newsCreated));
     }
 
     @Override
     public void deleteNews(Long id) {
         News newsEntity = getNewById(id);
         newsRepository.delete(newsEntity);
+        fileStore.deleteFilesFromS3Bucket("News-" + newsEntity.getId());
     }
 
     @Override
-    public NewsDto updateNews(Long id, NewsDto newsDto) {
+    public NewsResponseDto updateNews(Long id, NewsCreationDto newsCreationDto) {
 
-        News upNews = getNewById(id);
+        News newsUpdated = getNewById(id);
+        Categories categoriesEntity = categoriesService.findCategoriesById(newsCreationDto.getCategory());
 
-        if((newsDto.getCategory() == null)) {
-            newsDto.setCategory(upNews.getCategory());
+        newsUpdated.setCategory(categoriesEntity);
+        newsUpdated.setContent(newsCreationDto.getContent());
+        newsUpdated.setName(newsCreationDto.getName());
+        if(newsCreationDto.getImage() != null) {
+            uploadImage(newsCreationDto.getImage(), newsUpdated);
         }
-        if(!(newsDto.getCategory().getId() == null)) {
-            upNews.setCategory(newsDto.getCategory());
-        }
-        if(!newsDto.getName().isBlank()){
-            upNews.setName(newsDto.getName());
-        }
-        if(!newsDto.getContent().isBlank()){
-            upNews.setContent(newsDto.getContent());
-        }
-        if(!newsDto.getImage().isBlank()) {
-            upNews.setImage(newsDto.getImage());
-        }
-        upNews.setEdited(new Date());
-        return mapper.map(newsRepository.save(upNews), NewsDto.class);
-
+        newsUpdated.setEdited(new Date());
+        return projectionFactory.createProjection(NewsResponseDto.class, newsRepository.save(newsUpdated));
     }
 
     @Override
@@ -106,5 +116,14 @@ public class NewsImpl implements INews {
         );
 
         return newsRepository.findAllProjectedBy(pageable);
+    }
+
+
+    private void uploadImage(MultipartFile imageFile, News news) {
+        String path = String.format("%s/%s", bucketName, "News-" + news.getId());
+        String filename = String.format("%s-%s", imageFile.getOriginalFilename(), UUID.randomUUID());
+        fileStore.save(path, filename, imageFile);
+        String itemImageLink  = bucketUrl + "News-" + news.getId() + "/" + filename;
+        news.setImage(itemImageLink);
     }
 }
