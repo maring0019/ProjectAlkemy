b68f4884f4438e17652b547cacf1123f14e58159
Gabriel Andino <gabriel_andino@outlook.com>
2021-07-13 10:35:15 -0300

    Updating create a new image slide method

--- src/main/java/com/alkemy/ong/service/impl/ImgSlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/ImgSlideServiceImpl.java
@@ -61,7 +61,7 @@ public class ImgSlideServiceImpl implements IImgSlideService {
         );
 
         ImageSlide imageSlideCreated = imageRepo.save(imageSlideEntity);
-        uploadImage(imageSlideCreationDto, imageSlideCreated);
+        imageSlideCreated.setImageUrl(fileStore.save(imageSlideCreated, imageSlideCreationDto.getImage()));
         return mapper.map(imageRepo.save(imageSlideCreated), ImageSlideDto.class);
     }
 
@@ -78,17 +78,16 @@ public class ImgSlideServiceImpl implements IImgSlideService {
             imageSlide.setText(image.getText());
         if(image.getOrdered()!=null)
             imageSlide.setOrdered(image.getOrdered());
-        uploadImage(image, imageSlide);
-        ImageSlide imageSlideUpdated = imageRepo.save(imageSlide);
-        return mapper.map(imageSlideUpdated, ImageSlideDto.class);
+
+        imageSlide.setImageUrl(fileStore.save(imageSlide, image.getImage()));
+        return mapper.map(imageRepo.save(imageSlide), ImageSlideDto.class);
     }
 
     @Override
     public String deleteImage(Long id){
         ImageSlide imageSlide = getImageSlideById(id);
         imageRepo.delete(imageSlide);
-        //Va el nombre del segundo parametro de cuando se crea, osea el nombre del folder --> String path = String.format("%s/%s", bucketName, "Image-Slide-" + imageSlide.getId() + "/");
-        fileStore.deleteFilesFromS3Bucket("Image-Slide-" + imageSlide.getId()); //Le indicamos el nombre del folder
+        fileStore.deleteFilesFromS3Bucket(imageSlide);
         return messageSource.getMessage("slide.delete.successful", null, Locale.getDefault());
     }
 
@@ -104,22 +103,11 @@ public class ImgSlideServiceImpl implements IImgSlideService {
         return imageSlideCreationDtos;
     }
 
-
     @Override
     public ImageSlide getImageSlideById(Long id) {
         return imageRepo.findById(id).orElseThrow(() -> new EntityNotFoundException(
                 messageSource.getMessage("slide.error.not.found",null, Locale.getDefault())
         ));
     }
-
-    //Metodo privado de este service que consume el el servicio de amazon de subida de imagen
-    private void uploadImage(ImageSlideCreationDto imageSlideCreationDto, ImageSlide imageSlide) {
-        String path = String.format("%s/%s", bucketName, "Image-Slide-" + imageSlide.getId());
-        String filename = String.format("%s-%s", imageSlideCreationDto.getImage().getOriginalFilename(), UUID.randomUUID());
-        fileStore.save(path, filename, imageSlideCreationDto.getImage());
-        String itemImageLink  = bucketUrl + "Image-Slide-" + imageSlide.getId() + "/" + filename;
-        imageSlide.setImageUrl(itemImageLink);
-    }
-
 
 }
