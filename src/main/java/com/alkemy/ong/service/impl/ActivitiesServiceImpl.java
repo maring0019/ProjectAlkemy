e289e5f84c8984eb80b2c98b00017937963f35c7
Gabriel Andino <gabriel_andino@outlook.com>
2021-07-13 21:29:15 -0300

    Adding the upload image to aws s3 bucket method in create and update methods

--- src/main/java/com/alkemy/ong/service/impl/ActivitiesServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/ActivitiesServiceImpl.java
@@ -1,13 +1,14 @@
 package com.alkemy.ong.service.impl;
 
-import com.alkemy.ong.dto.ActivitiesDto;
+import com.alkemy.ong.dto.request.ActivityCreationDto;
+import com.alkemy.ong.dto.response.ActivityResponseDto;
 import com.alkemy.ong.model.Activity;
 import com.alkemy.ong.repository.ActivitiesRepository;
 import com.alkemy.ong.service.Interface.IActivities;
 import com.alkemy.ong.service.Interface.IFileStore;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
@@ -18,60 +19,56 @@ public class ActivitiesServiceImpl implements IActivities {
 
     private final ActivitiesRepository activitiesRepository;
     private final IFileStore fileStore;
-    private final ModelMapper mapper;
+    private final ProjectionFactory projectionFactory;
     private final MessageSource messageSource;
 
     @Autowired
-    public ActivitiesServiceImpl(ActivitiesRepository activitiesRepository, IFileStore fileStore, ModelMapper mapper, MessageSource messageSource) {
+    public ActivitiesServiceImpl(ActivitiesRepository activitiesRepository, IFileStore fileStore, ProjectionFactory projectionFactory, MessageSource messageSource) {
         this.activitiesRepository = activitiesRepository;
         this.fileStore = fileStore;
-        this.mapper = mapper;
+        this.projectionFactory = projectionFactory;
         this.messageSource = messageSource;
     }
 
+
     @Override
-    public ActivitiesDto createActivity(ActivitiesDto activitiesDto) {
+    public ActivityResponseDto createActivity(ActivityCreationDto activityCreationDto) {
 
         Activity activity = Activity.builder()
-                .name(activitiesDto.getName())
-                .content(activitiesDto.getContent())
-                .image(activitiesDto.getImage())
+                .name(activityCreationDto.getName())
+                .content(activityCreationDto.getContent())
                 .created(new Date())
                 .build();
-
-        return mapper.map(activitiesRepository.save(activity), ActivitiesDto.class);
+        Activity activityCreated = activitiesRepository.save(activity);
+        activityCreated.setImage(fileStore.save(activityCreated, activityCreationDto.getImage()));
+        return projectionFactory.createProjection(ActivityResponseDto.class, activitiesRepository.save(activityCreated));
     }
 
     @Override
-    public ActivitiesDto updateActivity(Long id, ActivitiesDto dto) {
+    public ActivityResponseDto updateActivity(Long id, ActivityCreationDto dto) {
         Activity activity = getActivityById(id);
 
         if(!dto.getName().isBlank())
         	activity.setName(dto.getName());
         if(!dto.getContent().isBlank())
         	activity.setContent(dto.getContent());
-        if(!dto.getImage().isBlank())
-        	activity.setImage(dto.getImage());
+        if(dto.getImage() != null)
+        	activity.setImage(fileStore.save(activity, dto.getImage()));
         
         activity.setEdited(new Date());
-        return mapper.map(activitiesRepository.save(activity), ActivitiesDto.class);
+        return projectionFactory.createProjection(ActivityResponseDto.class, activitiesRepository.save(activity));
     }
 
     @Override
-    public List<ActivitiesDto> getAllActivities() {
-        List<Activity> activityEntities = activitiesRepository.findAll();
-        List<ActivitiesDto> activitiesDto = new ArrayList<>();
-        activityEntities.forEach(a -> activitiesDto.add(mapper.map(a, ActivitiesDto.class)));
-        return activitiesDto;
+    public List<ActivityResponseDto> getAllActivities() {
+        return activitiesRepository.findAllProjectedBy();
     }
 
     @Override
     public void deleteActivity(Long id) {
         Activity activity = getActivityById(id);
         activity.setDeletedAt(new Date());
-        //Para eliminar las imagenes almacenadas en el s3 bucket le debo pasar el nombre del directorio donde estan las n imagenes
-        //No es que tiene n images, sino que al poner otra imagen esta se almacena en el mismo directorio por ende al eliminar la actividad eliminamos todas las imagenes que tuvo
-        fileStore.deleteFilesFromS3Bucket(activity.getId() + "-" + activity.getName().replaceAll("\\s", "-"));
+        fileStore.deleteFilesFromS3Bucket(activity);
         activitiesRepository.save(activity);
         activitiesRepository.delete(activity);
     }
