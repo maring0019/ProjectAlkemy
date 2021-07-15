10eb9c7727ec76cec9e0d129ea62e9b1771eacbd
Gabriel Andino <gabriel_andino@outlook.com>
2021-07-14 21:15:12 -0300

    Changing the return value from createComment method to CommentResponseDto

--- src/main/java/com/alkemy/ong/service/impl/CommentServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/CommentServiceImpl.java
@@ -1,44 +1,49 @@
 package com.alkemy.ong.service.impl;
 
-import org.modelmapper.ModelMapper;
+import com.alkemy.ong.dto.response.CommentResponseDto;
+import com.alkemy.ong.service.Interface.INewsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.stereotype.Service;
 
-import com.alkemy.ong.dto.CommentDto;
+import com.alkemy.ong.dto.request.CommentCreationDto;
 import com.alkemy.ong.model.Comment;
 import com.alkemy.ong.model.News;
 import com.alkemy.ong.model.User;
 import com.alkemy.ong.repository.CommentRepository;
-import com.alkemy.ong.repository.NewsRepository;
 import com.alkemy.ong.repository.UsersRepository;
 import com.alkemy.ong.service.Interface.ICommentService;
 
 @Service
 public class CommentServiceImpl implements ICommentService{
 
+	private final UsersRepository repoUser;
+	private final INewsService newsService;
+	private final ProjectionFactory projectionFactory;
+	private final CommentRepository repoComment;
+
 	@Autowired
-	private UsersRepository repoUser;
-	@Autowired
-	private NewsRepository repoNew;
-	@Autowired
-	private ModelMapper mapper;
-	@Autowired
-	private CommentRepository repoComment;
-	
-	
+	public CommentServiceImpl(UsersRepository repoUser, INewsService newsService, ProjectionFactory projectionFactory, CommentRepository repoComment) {
+		this.repoUser = repoUser;
+		this.newsService = newsService;
+		this.projectionFactory = projectionFactory;
+		this.repoComment = repoComment;
+	}
+
+
 	@Override
-	public CommentDto createComment(String email, CommentDto dto) {
+	public CommentResponseDto createComment(String email, CommentCreationDto dto) {
 		
 		User user = repoUser.findByEmail(email).get();
-		News post = repoNew.findById(dto.getIdNews()).get();
+		News post = newsService.getNewById(dto.getNews());
 		
-		Comment comment = new Comment();
-		comment.setNews(post);
-		comment.setUser(user);
-		comment.setBody(dto.getBody());
-		
-		
-		return mapper.map(repoComment.save(comment) , CommentDto.class);
+		Comment comment = new Comment(
+				user,
+				dto.getBody(),
+				post
+		);
+
+		return projectionFactory.createProjection(CommentResponseDto.class, repoComment.save(comment));
 	}
 
 	
