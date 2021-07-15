3594284f39a71deaac569633811459dcb18b51c9
Rodrigo Munafo <rodymuba@gmail.com>
2021-07-15 20:14:40 -0300

    Se agregan anotaciones Swagger a Dto Categories

--- src/main/java/com/alkemy/ong/dto/CategoriesNameDto.java
+++ src/main/java/com/alkemy/ong/dto/CategoriesNameDto.java
@@ -1,11 +1,14 @@
 package com.alkemy.ong.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
-
+@ApiModel(description = "Modelo de categories")
 @Data
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter
 public class CategoriesNameDto {
 
+    @ApiModelProperty(notes ="Nombre de categoria")
     private String name;
 }
