9966c7c589148d86d3cd3225e528afaf49072e2f
ToniSrt <4antoniogabriel20@gmail.com>
2021-07-19 22:32:13 -0300

    Initial commit, with only the firms

---
+++ src/test/java/com/alkemy/ong/controller/MemberControllerTest.java
@@ -0,0 +1,29 @@
+package com.alkemy.ong.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MemberControllerTest {
+
+	
+	@Test
+	public void createMemberTest() {
+		
+	}
+	
+	@Test
+	public void updateMemberTest() {
+		
+	}
+	
+	@Test
+	public void getAllMembersTest() {
+		
+	}
+	
+	@Test
+	public void deleteMemberTest() {
+		
+	}
+}
